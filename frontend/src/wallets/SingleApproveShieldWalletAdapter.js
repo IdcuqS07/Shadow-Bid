ed2981909f44fcdace6218606ab3fc4adc9d0e2f
IdcuqS07 <idchuq@gmail.com>
2026-03-28 17:27:32 +0800

    Harden Shield wallet connect deduping

--- frontend/src/wallets/SingleApproveShieldWalletAdapter.js
+++ frontend/src/wallets/SingleApproveShieldWalletAdapter.js
@@ -7,10 +7,13 @@ export class SingleApproveShieldWalletAdapter extends ShieldWalletAdapter {
     super(config);
 
     this._suppressAccountChangeUntil = 0;
-
-    const originalOnAccountChange = this._onAccountChange;
+    this._connectPromise = null;
+    this._lastKnownAddress = null;
 
     this._onAccountChange = () => {
+      const nextAddress = this._shieldWallet?.publicKey ?? null;
+      const currentAddress = this.account?.address ?? this._publicKey ?? this._lastKnownAddress ?? null;
+
       if (Date.now() < this._suppressAccountChangeUntil) {
         console.debug(
           '[SingleApproveShieldWalletAdapter] Ignoring immediate accountChanged event during connect grace window'
@@ -18,25 +21,64 @@ export class SingleApproveShieldWalletAdapter extends ShieldWalletAdapter {
         return;
       }
 
-      originalOnAccountChange?.();
+      if (!nextAddress) {
+        console.debug(
+          '[SingleApproveShieldWalletAdapter] Ignoring accountChanged event without a new public key'
+        );
+        return;
+      }
+
+      if (nextAddress === currentAddress) {
+        console.debug(
+          '[SingleApproveShieldWalletAdapter] Ignoring accountChanged event with the same address'
+        );
+        return;
+      }
+
+      console.debug(
+        '[SingleApproveShieldWalletAdapter] Updating account locally after accountChanged without reauthorizing'
+      );
+
+      this._publicKey = nextAddress;
+      this.account = { address: nextAddress };
+      this._lastKnownAddress = nextAddress;
+      this.emit('connect', this.account);
     };
   }
 
   async connect(network, decryptPermission, programs) {
+    if (this.connected && this.account?.address) {
+      return this.account;
+    }
+
+    if (this._connectPromise) {
+      return this._connectPromise;
+    }
+
     this._suppressAccountChangeUntil = Date.now() + INITIAL_CONNECT_ACCOUNT_CHANGE_GRACE_MS;
 
-    try {
-      const account = await super.connect(network, decryptPermission, programs);
-      this._suppressAccountChangeUntil = Date.now() + INITIAL_CONNECT_ACCOUNT_CHANGE_GRACE_MS;
-      return account;
-    } catch (error) {
-      this._suppressAccountChangeUntil = 0;
-      throw error;
-    }
+    this._connectPromise = super
+      .connect(network, decryptPermission, programs)
+      .then((account) => {
+        this._lastKnownAddress = account?.address ?? null;
+        this._suppressAccountChangeUntil = Date.now() + INITIAL_CONNECT_ACCOUNT_CHANGE_GRACE_MS;
+        return account;
+      })
+      .catch((error) => {
+        this._suppressAccountChangeUntil = 0;
+        throw error;
+      })
+      .finally(() => {
+        this._connectPromise = null;
+      });
+
+    return this._connectPromise;
   }
 
   async disconnect() {
     this._suppressAccountChangeUntil = 0;
+    this._connectPromise = null;
+    this._lastKnownAddress = null;
     return super.disconnect();
   }
 }
